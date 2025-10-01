3023f19a1f6c5165100794e64105f0f7124c748a
Mark Enfermo <markanthony.enfermo@gmail.com>
2025-10-02 01:07:12 +0800

    feat: enhance Home screen with categories, recommendations, and nearby stores sections

--- app/(consumers)/index.tsx
+++ app/(consumers)/index.tsx
@@ -1,10 +1,64 @@
-import { Text, View } from "react-native";
+import Card from "@/components/Card";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
-  
   return (
-    <View>
-      <Text>This is Home</Text>
-    </View>
-  )
+    <ScrollView style={styles.container}>
+      {/* Greetings */}
+      <View style={styles.section}>
+        <Text style={{ fontSize: 20, fontWeight: "600" }}>Hello, User</Text>
+        <Text>What would you like to shop today?</Text>
+      </View>
+      {/* Categories */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Categories</Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {[...Array(10)].map((_, index) => (
+            <Card key={index} style={styles.categoryCard}></Card>
+          ))}
+        </ScrollView>
+      </View>
+      {/* Recommended */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Recommended for you</Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {[...Array(10)].map((_, index) => (
+            <Card key={index} style={styles.recommendedSectionCard}></Card>
+          ))}
+        </ScrollView>
+      </View>
+      {/* Nearby Stores */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Nearby Stores</Text>
+        <View>
+          {[...Array(5)].map((_, index) => (
+            <Card key={index} style={styles.nearbyCard}></Card>
+          ))}
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15
+  },
+  section: {
+    marginVertical: 10,
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  categoryCard: {
+    borderRadius: "100%",
+    padding: 30
+  },
+  recommendedSectionCard: {
+    padding: 100,
+  },
+  nearbyCard: {
+    padding: 30,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
+    <View style={{ padding: 10 }}>
       <Text>Welcome to SugbuDeals</Text>
+      <Link href="/auth/login">Login</Link>
+      <Link href="/auth/register">Register</Link>
+      <Link href="/(consumers)">Consumer</Link>
     </View>
   );
 }
